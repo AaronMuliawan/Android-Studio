77b7b8955f4922350bf5c0b3effec89d44af4163
Aaron Muliawan <47096278+AaronMuliawan@users.noreply.github.com>
2019-08-21 23:48:39 +0700

    Tugas 2
    Intent

---
+++ Tugas2/app/src/main/java/com/example/tugas2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.tugas2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.btn_1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Rumus.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
